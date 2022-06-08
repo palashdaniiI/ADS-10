e750250105bfb968d4388dddd921ef274431b820
palashdaniiI <99319050+palashdaniiI@users.noreply.github.com>
2022-06-08 19:45:45 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -6,16 +6,16 @@
 class Tree {
  private:
     struct Node {
-        char imp;
+        char value;
         std::vector<Node*> rear;
     };
     Node* root;
     void Btr(Node* root, std::vector<char> number) {
         if (!number.size())
             return;
-        if (root->imp != '*') {
+        if (root->value != '*') {
             for (auto u = number.begin(); u != number.end(); u++)
-            if (*u == root->imp) {
+            if (*u == root->value) {
                 number.erase(u);
                 break;
             }
@@ -23,18 +23,18 @@ class Tree {
         for (size_t a = 0; a < number.size(); a++)
             root->rear.push_back(new Node());
         for (size_t a = 0; a < root->rear.size(); a++)
-            root->rear[a]->imp = number[a];
+            root->rear[a]->value = number[a];
         for (size_t a = 0; a < root->rear.size(); a++)
             Btr(root->rear[a], number);
     }
     std::vector<std::string> gree;
     void swap(Node* root, std::string str = "") {
         if (!root->rear.size()) {
-            str += root->imp;
+            str += root->value;
             gree.push_back(str);
         }
-        if (root->imp != '*')
-            str += root->imp;
+        if (root->value != '*')
+            str += root->value;
         for (size_t a = 0; a < root->rear.size(); a++)
             swap(root->rear[a], str);
     }
@@ -45,10 +45,10 @@ class Tree {
             return "";
         return gree[i];
     }
-    explicit Tree(std::vector<char> imp) {
+    explicit Tree(std::vector<char> value) {
         root = new Node();
-        root->imp = '*';
-        swap(root, imp);
+        root->value = '*';
+        Btr(root, value);
         swap(root);
     }
 };
